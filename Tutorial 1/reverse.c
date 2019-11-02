124edd10d68a0d4ad9c929fd7d15bb9b126bed06
abir <abirchandrabhushan@gmail.com>
2019-11-02 22:47:23 +0530

    update tutorial 1

---
+++ Tutorial 1/reverse.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+long long int reverse(long long int n) {
+  long long int result = 0;
+  _Bool negative = 0;
+  if(n < 0) {
+    negative = 1;
+    n = -n;
+}
+
+  while(n > 0) {
+    result *= 10;
+    result += n % 10;
+    n /= 10;
+  }
+  if(negative)
+    return -result;
+  return result;
+}
+
+int main(void) {
+  long long int n;
+  long long int result;
+  _Bool prime;
+
+  printf("Enter an integer\n");
+  scanf("%lli", &n);
+  printf("The reverse of %lli is %lli\n", n, reverse(n));
+
+  return 0;
+}
