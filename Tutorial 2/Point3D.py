d144bfb51e0971475eba10f47b7531f5dce6b227
abir <abirchandrabhushan@gmail.com>
2019-11-29 15:36:07 +0000

    update Point3D

--- Tutorial 2/Point3D.py
+++ Tutorial 2/Point3D.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
+
+
 class Point3D(object):
-  def __init__(self, x, y, z):
-    self.x = x
-    self.y = y
-    self.z = z
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-  def print_point_info(self):
-    print("(%d, %d, %d)" % (self.x, self.y, self.z))
+    def print_point_info(self):
+        print("(%d, %d, %d)" % (self.x, self.y, self.z))
 
-my_point = Point3D(1, 2, 3)
-my_point.print_point_info()
+
+if __name__ == "__main__":
+    my_point = Point3D(1, 2, 3)
+    my_point.print_point_info()
